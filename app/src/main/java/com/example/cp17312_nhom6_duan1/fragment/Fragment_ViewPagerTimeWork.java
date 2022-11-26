75a07d22b376bbee67dd55ca2b780ab313af767a
phamdat23 <112074117+phamdat23@users.noreply.github.com>
2022-11-26 14:04:13 +0700

    sua tablayout time work

--- app/src/main/java/com/example/cp17312_nhom6_duan1/fragment/Fragment_ViewPagerTimeWork.java
+++ app/src/main/java/com/example/cp17312_nhom6_duan1/fragment/Fragment_ViewPagerTimeWork.java
@@ -1,10 +1,14 @@
 package com.example.cp17312_nhom6_duan1.fragment;
 
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TableLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,24 +22,27 @@ import com.google.android.material.tabs.TabLayoutMediator;
 
 public class Fragment_ViewPagerTimeWork extends Fragment {
     private ViewPager2 vpTimeWork;
-    private TabLayout tabTimeWork;
     AdapterViewPagerTimeWork adapterViewPagerTimeWork;
+    private TabLayout tabTimeWork;
+
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.fragment_viewpager_time_work, container, false);
+        View view = inflater.inflate(R.layout.fragment_viewpager_time_work, container, false);
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        tabTimeWork = view.findViewById(R.id.tab_time_work);
         vpTimeWork = view.findViewById(R.id.vp_time_work);
         adapterViewPagerTimeWork = new AdapterViewPagerTimeWork(this);
         vpTimeWork.setAdapter(adapterViewPagerTimeWork);
-        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabTimeWork, vpTimeWork, new TabLayoutMediator.TabConfigurationStrategy() {
+        tabTimeWork = view.findViewById(R.id.tab_time_work);
+        TabLayoutMediator mediator = new TabLayoutMediator(tabTimeWork, vpTimeWork, new TabLayoutMediator.TabConfigurationStrategy() {
             @Override
             public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
                 if(position==0){
@@ -45,7 +52,8 @@ public class Fragment_ViewPagerTimeWork extends Fragment {
                 }
             }
         });
-        tabLayoutMediator.attach();
-
+        mediator.attach();
     }
+
+
 }
