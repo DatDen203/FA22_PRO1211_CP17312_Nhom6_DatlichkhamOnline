3cfaa309f417390a16542107da336367da457178
LuanNT <thanhluan2818@gmail.com>
2022-12-02 09:46:57 +0700

    luan sua lai luu dang nhap

--- app/src/main/java/com/example/cp17312_nhom6_duan1/fragment/fragment_doctor/FragmentInforDoctor.java
+++ app/src/main/java/com/example/cp17312_nhom6_duan1/fragment/fragment_doctor/FragmentInforDoctor.java
@@ -1,22 +1,41 @@
 package com.example.cp17312_nhom6_duan1.fragment.fragment_doctor;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.cp17312_nhom6_duan1.R;
+import com.example.cp17312_nhom6_duan1.SignInActivity;
 
 
 public class FragmentInforDoctor extends Fragment {
+     ImageView imgLogOut;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_infor_doctor, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        imgLogOut = (ImageView) view.findViewById(R.id.img_log_out);
+        imgLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), SignInActivity.class));
+                getActivity().finish();
+            }
+        });
     }
 }
